165b812203349e81f75266a50e9a2e4c6cad9909
wb <wb@localhost.localdomain>
2022-05-19 15:49:08 +0800

    按照模板吸怪

--- e1/test/t_client.cpp
+++ e1/test/t_client.cpp
@@ -1,73 +1,49 @@
-#include <iostream>
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#include <iostream>
+#include <string>
 
 
 #define BUF_SIZE 1024
 
 int main() {
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	int sfd, s;
-	size_t len;
-	ssize_t nread;
-	char buf[BUF_SIZE];
 
-
-	std::cout << "Begin tcp client" << std::endl;
-	/* Obtain address(es) matching host/port. */
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;    /* Allow IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_STREAM; /* stream socket */
-	hints.ai_flags = 0;
-	hints.ai_protocol = 0;          /* Any protocol */
-
-	s = getaddrinfo("127.0.0.1", "65530", &hints, &result);
-	if (s != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == client_fd) {
+		perror("创建socket失败");
 		exit(1);
 	}
 
-	/* getaddrinfo() returns a list of address structures.
-	 *               Try each address until we successfully connect(2).
-	 *                             If socket(2) (or connect(2)) fails, we (close the socket
-	 *                                           and) try the next address. */
-
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,
-				rp->ai_protocol);
-		if (sfd == -1)
-			continue;
-
-		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-			break;                  /* Success */
-
-		close(sfd);
-	}
-
-	freeaddrinfo(result);           /* No longer needed */
-
-	if (rp == NULL) {               /* No address succeeded */
-		fprintf(stderr, "Could not connect\n");
-		exit(1);
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(65530);
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
+	
+	//connect 0 success, -1 fail
+	if (connect(client_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1 ) {
+		perror("连接失败");
+		exit(1);	
 	}
 
 	//std::cin 如何从标准输入读取数据
 	std::string str;
+	char buf[BUF_SIZE];
 	while(std::cin >> str) {
-		send(sfd, str.c_str(), str.size(), 0);
-		int nread = read(sfd, buf, 1024);
+		send(client_fd, str.c_str(), str.size(), 0);
+		int nread = read(client_fd, buf, BUF_SIZE);
 		if (nread <= 0)
 			break;
-		//buf[nread] = '\0';
+		buf[nread] = '\0';
 		std::cout << buf << std::endl;
 	}
+
+	close(client_fd);
 
 	return 0;
 }
--- e1/test/t_server.cpp
+++ e1/test/t_server.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -7,53 +5,84 @@
 #include <unistd.h>
 
 
+#include <iostream>
+
+
 int main() {
 	std::cout << "Begin tcp server" << std::endl;
 
-	int fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (fd <= 0) {
+	//打开一个socket tcp的fd
+	/*
+	 *  #include <sys/socket.h>
+	 * int socket(int domain, int type, int protocol);
+	 * return -1 就表示出错。其他情况是一个文件描述符
+	 * 
+	 */
+	int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_fd == 1) {
 		perror("socket error");
 		exit(1);
 	}
-	std::cout <<fd << std::endl;
+	std::cout << listen_fd << std::endl;
 
-	struct sockaddr_in addr;
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(65530);
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/*
+	 * #include<netinet/in.h>
+	 * sockaddr_in 是internet环境下套接字的地址形式
+	 struct sockaddr_in {
+	 	short   sin_family;         //address family
+	 	u_short sin_port;           //16 bit TCP/UDP port number
+	 	struct  in_addr sin_addr;   //32 bit IP address
+	 	char    sin_zero[8];        //not use, for align
+	 };
+	 这个是可以和sockaddr 互相强制转换的
+	 https://blog.csdn.net/ZWE7616175/article/details/80252048
+	 */
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(65530);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	//ipv4
 	//inet_aton("127.0.0.1", &addr.sin_addr);
 
-	if (bind(fd, (struct sockaddr *) &addr,  sizeof(addr)) == -1) {
+	//bind 成功返回0，失败返回-1
+	if (bind(listen_fd, (struct sockaddr *) &serv_addr,  sizeof(serv_addr)) == -1) {
 		perror("bind error");
 		exit(1);
 	}
 
-	int max_conn = 3;
+	int max_conn = 10;
 
-	if (listen(fd, max_conn) == -1) {
+	//listen 成功返回0，失败返回-1
+	//第二个代表等待队列的长度
+	if (listen(listen_fd, max_conn) == -1) {
 		perror("listen error");
 		exit(1);
 	}
 
 	while(1) {
-		struct sockaddr_in clientAddr;
-		socklen_t  addrLen = sizeof(clientAddr);
-		int connFd = accept(fd, (struct sockaddr *) &clientAddr, &addrLen);
-		if (connFd <= 0) {
+		struct sockaddr_in cli_addr;
+		socklen_t len = sizeof(cli_addr);
+		//成功返回一个非负的整数，代表文件描述符(0 也是正确的)，失败返回-1
+		int conn_fd = accept(listen_fd, (struct sockaddr *) &cli_addr, &len);
+		if (conn_fd == -1) {
 			perror("accept error");
-			break;
+			exit(1);
 		}
+		//放到外面
+		char buf[1024];
 		while (1) {
-			char data[1024];
-			size_t n = read(connFd, data, 1024);
-			if (n <= 0) {
+			int nread = read(conn_fd, buf, 1024);
+			if (nread <= 0) {
 				break;
 			}
-			send(connFd, data, n, 0);
+			send(conn_fd, buf, nread, 0);
 		}
+		//不要忘了close
+		close(conn_fd);
 	}
+	//不要往了close
+	close(listen_fd);
 
 	return 0;
 }
